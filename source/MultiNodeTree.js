9d592966feb0a42b46a94438552b1649e29deed5
Your Real Name <me@example.com>
2017-08-29 18:06:31 +0800

    kill async/await

--- source/MultiNodeTree.js
+++ source/MultiNodeTree.js
@@ -1,7 +1,8 @@
-import  util from 'util';
+//import util from 'util';
 import SymbolTree from 'symbol-tree';
 import assert from 'assert';
 import MyLinkList from './MyLinkList';
+
 
 export default class MultiNodeTree {
   constructor() {
@@ -11,267 +12,305 @@ export default class MultiNodeTree {
     };
     this.myMap = new Map();
     this.indexMap = new Map();
-    this.TreeArray = this.tree.treeToArray(this.root);
+    //this.TreeArray = this.tree.treeToArray(this.root);
   }
-  static async printTest(){
+  static printTest() {
     console.log('MultiNodeTree printTest');
   }
 
-  static async buildTree(arr) {
+  buildTree(arr) {
     const myList = new MyLinkList();
 
-    arr.forEach((i) => {
-      myList.insert(i.id);
-    });
-    arr.forEach((i) => {
-      myList.removeNode(i.sub);
-    });
+    Promise.all(arr.map((e) => {
+      myList.insert(e.id);
+    }));
+    Promise.all(arr.map((e) => {
+      myList.removeNode(e.sub);
+    }));
 
 
     const {
       data,
-    } = await myList.getHeadNode();
-    // console.log(data);
-    // console.log(await myList.getSize());
-    // data = await myList.getlist();
+    } = myList.getHeadNode();
 
-    console.log('myTree----------------');
     const myTree = new MultiNodeTree();
-    await myTree.setRoot(data);
-    const root = await myTree.getRoot();
-    // console.log(root);
+    this.setRoot(data);
+    const root = this.getRoot();
 
     const stack = [];
     stack.push(root);
-    // console.log('root %j' ,root);
 
-    for (var i = 0; i < arr.length; i++) {
-      var {
+    Promise.all(arr.map((e) => {
+      let {
         id,
         sub,
-      } = arr[i];
-
+      } = e;
       if (id === root.key) {
-        /*
-          console.log('root %j', root);
-          console.log('id %j', id);
-          console.log('sub %j', sub);
-  */
         stack.push(sub);
-        await myTree.addChild(id, sub);
+        this.addChild(id, sub);
       }
-    }
+    }));
 
-    // await myTree.printTree();
+
+    //  this.printTree();
     while (stack.length > 0) {
       // console.log('stack.length '+stack.length);
       const node = stack.pop();
-      for (var i = 0; i < arr.length; i++) {
-        var {
+      Promise.all(arr.map((e) => {
+        let {
           id,
           sub,
-        } = arr[i];
-        // console.log('1 node '+util.inspect(node, false, null));
-        // console.log(node);
-        // console.log('3 id %s node %j',id,node);
-
-        if (id == node) {
+        } = e;
+        if (id === node) {
           stack.push(sub);
-          // console.log('node %j',node);
-          // console.log('sub %j',sub);
-          await myTree.addChild(node, sub);
+          this.addChild(node, sub);
         }
-      }
-    }
+      }));
 
-    await myTree.caculateIndex();
-    // await myTree.printTree();
-    return await myTree.getTreeArr();
+    }
+    this.caculateIndex();
+    //  this.printTree();
+    return this.getTreeArr();
   }
 
-  async setRoot(key) {
-    var root = {
+  static buildTree2(arr) {
+    const myList = new MyLinkList();
+
+    Promise.all(arr.map((e) => {
+      myList.insert(e.id);
+    }));
+    Promise.all(arr.map((e) => {
+      myList.removeNode(e.sub);
+    }));
+
+
+    const {
+      data,
+    } = myList.getHeadNode();
+
+    const myTree = new MultiNodeTree();
+    myTree.setRoot(data);
+    const root = myTree.getRoot();
+
+    const stack = [];
+    stack.push(root);
+
+    Promise.all(arr.map((e) => {
+      let {
+        id,
+        sub,
+      } = e;
+      if (id === root.key) {
+        stack.push(sub);
+        myTree.addChild(id, sub);
+      }
+    }));
+
+
+    //  myTree.printTree();
+    while (stack.length > 0) {
+      const node = stack.pop();
+      Promise.all(arr.map((e) => {
+        let {
+          id,
+          sub,
+        } = e;
+        if (id === node) {
+          stack.push(sub);
+          myTree.addChild(node, sub);
+        }
+      }));
+
+    }
+    myTree.caculateIndex();
+    //  myTree.printTree();
+    return myTree.getTreeArr();
+  }
+
+  setRoot(key) {
+    let root = {
       key: key
     }
     this.root = root;
-    //console.log('root %j',root);
     this.myMap.set(root.key, 0);
-    //console.log('setRoot '+util.inspect(root, false, null));
     this.tree.prependChild(this.parent, root);
-    //console.log('setRoot ok'+util.inspect(root, false, null));       
-
-    //console.log('setRoot ok'+util.inspect(this.arr[{key:key}], false, null));
   }
 
-  async getRoot() {
+  getRoot() {
     return this.tree.firstChild(this.parent);
   }
 
-  async addChild(parent, child) {
-    //console.log(typeof parent);
-    //console.log(typeof child);
+  addChild(parent, child) {
+    
     if (typeof parent != "string") {
-      console.log("typeof parent != string");
-      console.log(typeof parent);
-      return;
+      throw new TypeError('getNodeByKey typeof parent != string')
     }
     if (typeof child != "string") {
-      console.log("typeof child != string");
-      console.log(typeof child);
-      return;
+      throw new TypeError('getNodeByKey typeof child != string')
     }
-    var pKey = {
+    let pKey = {
       key: parent
     };
-    var cKey = {
+    let cKey = {
       key: child
     };
-    //console.log(parent);
-    //console.log(child);
-    var nodeParent = await this.getNodeByKey(parent);
+    let nodeParent = this.getNodeByKey(parent);
     if (nodeParent == null) {
       console.log('nodeParent==null');
       return;
     }
-    var nodeOldParent = await this.getParentByKey(child);
-    //console.log(nodeParent);
-    //console.log(nodeOldParent);
+    
+    let nodeOldParent = this.getParentByKey(child);
     if (nodeOldParent != null && nodeParent != null) {
 
-      var nodeParentDepth = await this.getDepthByKey(nodeParent.key);
-      var nodeOldParentDepth = await this.getDepthByKey(nodeOldParent.key);
+      let nodeParentDepth = this.getDepthByKey(nodeParent.key);
+      let nodeOldParentDepth = this.getDepthByKey(nodeOldParent.key);
       if (nodeOldParentDepth >= nodeParentDepth) {
-        console.log("nodeOldParentDepth>=nodeParentDepth %d %d", nodeOldParentDepth, nodeParentDepth);
         return;
       } else {
-        await this.removeNodeByKey(child);
+        this.removeNodeByKey(child);
       }
     }
-    //        this.arr[cKey] = cKey;
-    //var dep=this.myMap.get(pKey.key)+1;
-    var dep = await this.getDepthByKey(pKey.key) + 1;
+    let dep = this.getDepthByKey(pKey.key) + 1;
     this.myMap.set(cKey.key, dep)
-    //      this.arrDepth[cKey] = this.arrDepth[pKey] + 1;
     this.tree.appendChild(nodeParent, cKey);
-    var parent1 = this.tree.parent(cKey);
+    let parent1 = this.tree.parent(cKey);
   }
 
-  async removeNodeByKey(key) {
-    var node = await this.getNodeByKey(key);
+  removeNodeByKey(key) {
+    let node = this.getNodeByKey(key);
     if (node != null) {
       this.tree.remove(node);
     }
   }
-  async getNodeByKey(key) {
-
+  getNodeByKey(key) {
+    let result = null;
     if (typeof key != "string") {
-      console.log("getNodeByKey typeof key != string");
-      console.log(key);
-      console.log(typeof key);
-      assert.deepEqual(typeof key == "string");
-      return null;
+      throw new TypeError('getNodeByKey typeof key != string')
     }
 
     this.TreeArray = this.tree.treeToArray(this.root);
-    //console.log('this.TreeArray.length %d' ,this.TreeArray.length); 
-    for (var i = 0; i < this.TreeArray.length; i++) {
-      //console.log('this.TreeArray[i].key %s' ,this.TreeArray[i].key); 
-      //console.log('key %s' ,key); 
+    for (let i = 0; i < this.TreeArray.length; i++) {
       if (this.TreeArray[i].key == key) {
-        //console.log('getNodeByKey arr[i] %j , key %s' ,this.TreeArray[i],key); 
-        var ppp = this.tree.parent(this.TreeArray[i])
-        //console.log('getNodeByKey ppp %j' ,ppp); 
-        return this.TreeArray[i];
+        result = this.TreeArray[i];
+        break;
       }
     }
-    return null;
+    return result;
   }
-  async getDepthByKey(key) {
-    var node = await this.getNodeByKey(key);
+
+  getDepthByKey(key) {
+    let node = this.getNodeByKey(key);
     if (node == null) return -1;
     return this.myMap.get(node.key);
   }
-  async setDepthByKey(key, d) {
-    //var node = await this.getNodeByKey(key);
+  setDepthByKey(key, d) {
+    //let node =  this.getNodeByKey(key);
     //if(node==null) return -1;
     return this.myMap.set(key, d);
   }
-  async getIndexByKey(key) {
-    var node = await this.getNodeByKey(key);
+
+  getIndexByKey(key) {
+    let node = this.getNodeByKey(key);
     if (node == null) return -1;
     return this.indexMap.get(node.key);
   }
-  async getParentIndexByKey(key) {
-    var node = await this.getParentByKey(key);
-    if (node == null) return -1;
-    return this.indexMap.get(node.key);
+
+  getParentIndexByKey(key) {
+    let node = this.getParentByKey(key);
+    if (node == null) {
+      //runtime error
+      return -1;
+    }
+    const indexOfParent = this.indexMap.get(node.key)
+    return indexOfParent;
   }
-  async setIndexByKey(key, d) {
-    //var node = await this.getNodeByKey(key);
-    //if(node==null) return -1;
-    return this.indexMap.set(key, d);
+
+  getMyOrderByKey(key) {
+    let node = this.getParentByKey(key);
+
+    if (node == null) {
+      return -1;
+    }
+    const childrenArray = this.tree.childrenToArray(node);
+    let myOrder = 0;
+    let counter = 0;
+    childrenArray.forEach(j => {
+
+      if (j.key == key) {
+        myOrder = counter;
+      }
+      counter++;
+    })
+    return myOrder;
   }
-  async getMaxChildNumber() {
+
+  setIndexByKey(key, d) {
+    this.indexMap.set(key, d);
+  }
+
+  getMaxChildNumber() {
     this.TreeArray = this.tree.treeToArray(this.root);
-    var childMax = 0;
-    for (var i = 0; i < this.TreeArray.length; i++) {
-      var childNum = this.tree.childrenCount(this.TreeArray[i]);
+    let childMax = 0;
+    for (let i = 0; i < this.TreeArray.length; i++) {
+      let childNum = this.tree.childrenCount(this.TreeArray[i]);
       if (childNum > childMax) childMax = childNum;
     }
     return childMax;
   }
-  async caculateIndex() {
-    var childMax = await this.getMaxChildNumber();
+
+  caculateIndex() {
+    var childMax = this.getMaxChildNumber();
     this.TreeArray = this.tree.treeToArray(this.root);
 
     var rootNode = this.TreeArray[0];
-    await this.setIndexByKey(rootNode.key, 0);
+    this.setIndexByKey(rootNode.key, 0);
     for (var j of this.tree.treeIterator(rootNode)) {
-      if (await this.getIndexByKey(j.key) == 0) {
+      if (this.getIndexByKey(j.key) == 0) {
         continue;
       }
       var previousSibling = this.tree.previousSibling(j);
       if (previousSibling) {
-        var psIndex = await this.getIndexByKey(previousSibling.key)
-        await this.setIndexByKey(j.key, psIndex + 1);
+        var psIndex = this.getIndexByKey(previousSibling.key)
+        this.setIndexByKey(j.key, psIndex + 1);
         continue;
       }
       var order = 0;
 
-      var parentIndex = await this.getParentIndexByKey(j.key);
+      var parentIndex = this.getParentIndexByKey(j.key);
       var myIndex = parentIndex * childMax + 1 + order;
       order = order + 1;
-      await this.setIndexByKey(j.key, myIndex);
+      this.setIndexByKey(j.key, myIndex);
     }
   }
 
-  async getParentByKey(key) {
-    var node = await this.getNodeByKey(key);
+  getParentByKey(key) {
+    let node = this.getNodeByKey(key);
+
+
     if (node == null) return null;
     return this.tree.parent(node)
   }
-  async printTree() {
+  printTree() {
     console.log('printTree');
     this.TreeArray = this.tree.treeToArray(this.root);
-    await this.printArr(this.TreeArray);
+    this.printArr(this.TreeArray);
   }
-  async getSize() {
+  getSize() {
     this.TreeArray = this.tree.treeToArray(this.root);
     return this.TreeArray.length;
   }
 
-  async getTreeArr() {
+  getTreeArr() {
     const arr2 = [];
     const arr = this.tree.treeToArray(this.root);
     for (let i = 0; i < arr.length; i++) {
       const key = arr[i].key;
-      const node = await this.getNodeByKey(key);
-      const index = await this.getIndexByKey(key);
-      // console.log('node %j',node);
-      const parent = await this.getParentByKey(key);
+      const node = this.getNodeByKey(key);
+      const index = this.getIndexByKey(key);
+      const parent = this.getParentByKey(key);
       let pkey = '0';
       if (parent) pkey = parent.key;
-      // console.log('parent %j',parent);
       arr2.push({
         parent: pkey,
         key,
@@ -283,16 +322,14 @@ export default class MultiNodeTree {
   }
 
 
-  async printArr(arr) {
-    for (var i = 0; i < arr.length; i++) {
-      var key = arr[i].key;
-      var node = await this.getNodeByKey(key)
-      var index = await this.getIndexByKey(key)
-      //console.log('node %j',node);
-      var parent = await this.getParentByKey(key);
-      var pkey = '0';
+  printArr(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      let key = arr[i].key;
+      let node = this.getNodeByKey(key)
+      let index = this.getIndexByKey(key)
+      let parent = this.getParentByKey(key);
+      let pkey = '0';
       if (parent) pkey = parent.key;
-      //console.log('parent %j',parent);
       console.log({
         parent: pkey,
         key: key,
